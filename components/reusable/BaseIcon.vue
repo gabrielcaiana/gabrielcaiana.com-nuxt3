31cec828095d5a40127991332e5dff85123e66b1
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-12 19:52:57 -0300

    chore: create reusable components

---
+++ components/reusable/BaseIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <i
+    :height="size"
+    :width="size"
+    :data-feather="name"
+    :class="$attrs.class"
+  ></i>
+</template>
+
+<script setup>
+import feather from 'feather-icons'
+import { defineProps, onMounted, onUpdated } from 'vue'
+
+defineProps({
+  name: {
+    type: String,
+    default: null,
+  },
+  size: {
+    type: String,
+    default: '24',
+  },
+})
+
+onMounted(() => {
+  feather.replace()
+})
+
+onUpdated(() => {
+  feather.replace()
+})
+</script>
---
+++ components/reusable/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>{{ title }}</button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
