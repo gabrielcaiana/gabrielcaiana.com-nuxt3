2cb4b08a26aca94ea160ce09485952e632dbbda7
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-12 01:14:48 -0300

    add head in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,29 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Gabriel Caiana - Website',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+    },
+  },
+
+  loading: {
+    color: 'green',
+    height: '5px',
+    failedColor: 'red',
+    continuous: true,
+  },
+
   css: ['@/assets/css/styles.css'],
   build: {
     postcss: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  purge: [
+  content: [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    // './nuxt.config.{js,ts}',
   ],
   darkMode: 'class',
   theme: {
