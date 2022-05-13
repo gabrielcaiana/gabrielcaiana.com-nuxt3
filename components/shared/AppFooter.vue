261910116e4e1444c8924c50b45cf2aa24449561
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-12 21:13:33 -0300

    config appFooter

---
+++ components/shared/AppFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mx-auto">
+    <div
+      class="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark"
+    >
+      <div class="flex flex-col justify-center items-center mb-12 sm:mb-20">
+        <p
+          class="font-general-semibold text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-5"
+        >
+          Siga-me
+        </p>
+        <ul class="flex gap-4 sm:gap-8">
+          <a
+            v-for="social in socialProfile"
+            :key="social.id"
+            :href="social.url"
+            target="__blank"
+            class="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-500"
+          >
+            <BaseIcon :name="social.icon" class="w-6 sm:w-8 h-6 sm:h-8" />
+          </a>
+        </ul>
+      </div>
+
+      <FooterCopyright />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import FooterCopyright from './FooterCopyright.vue'
+import { useSocialProfile } from '@/stores/socialProfile'
+import BaseIcon from '@/components/reusable/BaseIcon.vue'
+
+const { socialProfile } = useSocialProfile()
+</script>
