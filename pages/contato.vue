663df1a3a9177e783a110051722e0ef651ffd26f
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-05-14 21:15:49 -0300

    chore: create contact page

--- pages/contato.vue
+++ pages/contato.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>contato</div>
+  <div
+    class="container mx-auto flex flex-col-reverse md:flex-row md:py-10 md:mt-20"
+  >
+    <ContactForm />
+
+    <ContactDetails :contacts="contacts" />
+  </div>
 </template>
 
 <script>
-export default {}
+import ContactForm from '~/components/contact/ContactForm.vue'
+export default {
+  components: {
+    ContactForm,
+  },
+  data: () => {
+    return {
+      contacts: [
+        {
+          id: 1,
+          name: 'São Paulo, Brasil',
+          icon: 'map-pin',
+        },
+        {
+          id: 2,
+          name: 'gabrielcaianaguedes@gmail.com',
+          icon: 'mail',
+        },
+        {
+          id: 3,
+          name: '11 94987-5163',
+          icon: 'phone',
+        },
+      ],
+    }
+  },
+}
 </script>
